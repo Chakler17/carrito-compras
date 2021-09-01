dce68a663be6130e7ae179fbc617d1daa331cc55
Hugo Arias <arias17f@gmail.com>
2021-09-01 08:12:01 -0500

    First commit project carrito

---
+++ js/app.js
@@ -0,0 +1,133 @@
+// Variables
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+const carritoCount = document.getElementById("carrito-count");
+let articulosCarrito = [];
+
+function actualizarContador() {
+    if (articulosCarrito.length === 0) {
+      carritoCount.style.display = "none";
+    } else {
+      carritoCount.innerText = articulosCarrito.length;
+    }
+  }
+
+cargarEventListeners();
+function cargarEventListeners() {
+  // Cuando agregas un curso presionando "Agregar al carrito"
+  listaCursos.addEventListener("click", agregarCurso);
+
+  // Elimina cursos del carrito
+  carrito.addEventListener("click", eliminarCurso);
+
+  // Vaciar el carrito
+  vaciarCarritoBtn.addEventListener("click", () => {
+    articulosCarrito = []; // Reseteamos el arreglo
+
+    limpiarHTML(); // Eliminamos todo el HTML
+
+    actualizarContador(); // Actualiza el contador de carrito
+  });
+}
+
+// Funciones
+function agregarCurso(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    // Obetenemos el elemento padre del boton (card)
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
+
+    actualizarContador(); // Actualiza el contador de carrito
+  }
+}
+
+function eliminarCurso(e) {
+  if (e.target.classList.contains("borrar-curso")) {
+    const cursoId = e.target.getAttribute("data-id");
+
+    // ELiminar curso del carrito articulosCarrito por el data-id
+    articulosCarrito = articulosCarrito.filter((curso) => curso.id !== cursoId);
+
+    carritoHTML(); // Iterar sobre el carrito y mostrar su HTML
+
+    actualizarContador(); // Actualiza el contador de carrito
+  }
+}
+
+// Leer el contenido del HTML al que le dimos click y extrae la informacion del curso
+
+function leerDatosCurso(curso) {
+  // console.log(curso);
+
+  // Crear un objeto con el contenido del curso actual
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector("span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
+
+  // Revisa si un elemento ya existe en el carrito
+  const existe = articulosCarrito.some((curso) => curso.id === infoCurso.id);
+  if (existe) {
+    // Actualizamos la cantidad
+    const cursos = articulosCarrito.map((curso) => {
+      if (curso.id === infoCurso.id) {
+        curso.cantidad++;
+        return curso; // Retorna el objeto actualizados
+      } else {
+        return curso; // Retorna los objetos que no son los duplicados
+      }
+    });
+    articulosCarrito = [...cursos];
+  } else {
+    // Agregar elementos al arreglo de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+  }
+
+  carritoHTML();
+}
+
+// Muestra el carrito de compras en el HTML
+function carritoHTML() {
+  // Limpiar el HTML
+  limpiarHTML();
+
+  // Recorre el carrito y genera HTML
+
+  articulosCarrito.forEach((curso) => {
+    const { imagen, titulo, precio, cantidad, id } = curso;
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td><img src="${imagen}" width="100"></td>
+        <td>${titulo}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+        <td>
+        <a href="#" class="borrar-curso" data-id="${id}">X</a>
+        </td>
+        `;
+
+    // Agrega el HTML del carrito en el tbody
+    contenedorCarrito.appendChild(row);
+  });
+}
+
+// Eliminar los cursos del tbody
+function limpiarHTML() {
+  //   Forma lenta
+  //   contenedorCarrito.innerHTML = "";
+
+  while (contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+  }
+}
+
+// Contador articulos de carrito
+
+
+console.log(articulosCarrito.length);
